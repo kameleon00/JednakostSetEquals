74ec41e36d29a693831bb5f2bccaed20257b3dba
Kameleon <kameleon@localhost.localdomain>
2021-01-07 20:45:50 +0100

    Initial commit

---
+++ JednakostSetEquals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JednakostSetEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> gradovi = new HashSet<string>() {"Beograd", "Nis", "Kragujevac", "Subotica", "Novi Sad"};
+            HashSet<string> gradoviNaBalkanu = new HashSet<string>() {"Zagreb", "Beograd", "Kragujevac", "Podgorica", "Novi Sad"};
+
+            string[] velikiGradovi = new string[] {"Nis", "Kragujevac", "Novi Sad", "Subotica", "Beograd"};
+            
+            Console.WriteLine("gradovi SetEquals gradoviNaBalkanu: " + gradovi.SetEquals(gradoviNaBalkanu));//false jer ima razlike
+            Console.WriteLine("gradoviUSrbiji SetSquals velikiGradovi: " + gradovi.SetEquals(velikiGradovi));//true jer nema
+
+            Console.ReadLine();
+        }
+    }
+}
